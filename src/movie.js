fac231eda3e44acd748c4c0ad1f14b9c6c2daa81
scseong <goidtv@naver.com>
2023-10-25 16:41:40 +0900

    chore: 영화 API  popular로 변경 (#6)

--- src/movie.js
+++ src/movie.js
@@ -2,6 +2,8 @@
 import { fetchMovies } from './apis.js';
 
 const $movieList = document.querySelector('.movie-list');
+const POPULAR_URL =
+  'https://api.themoviedb.org/3/movie/popular?language=en-US&page=1';
 
 const handleClickCard = (event) => {
   if (event.target === $movieList) return;
@@ -13,9 +15,7 @@ const handleClickCard = (event) => {
   }
 };
 
-export const generateMovieCards = async (
-  apiUrl = 'https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1',
-) => {
+export const generateMovieCards = async (apiUrl = POPULAR_URL) => {
   const { results: movies } = await fetchMovies(apiUrl);
 
   $movieList.innerHTML = movies
@@ -24,7 +24,7 @@ export const generateMovieCards = async (
         <img src=${'https://image.tmdb.org/t/p/w300' + movie['poster_path']}>
         <h5>${'⭐ Rating: ' + movie['vote_average']}</h5>
         <h2>${movie['title']}</h2>
-        <p>${'⭐ Rating: ' + movie['overview']}</p>
+        <p>${movie['overview']}</p>
       </li>
       `;
     })
