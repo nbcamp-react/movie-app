370ad28acbc3781222cad63277cac2281420280b
hosu kang <rkdghtn6036@gmail.com>
2023-10-27 18:39:51 +0900

    댓글 기능 구현 (#32)

--- src/comment_login.js
+++ src/comment_login.js
@@ -1,120 +1,88 @@
-// // const commentBtn = document.querySelector("#commentFrm");
-// // const list = [];
+const commentBtn = document.querySelector('.submit'); // 등록 버튼
+const commentList = document.querySelector('.comment-list'); // 댓글 리스트
+const total = document.querySelector('.count'); //댓글 수
+const commentInput = document.querySelector('.comment-input'); //댓글 입력
+const nickInput = document.querySelector('.user-nick input'); // 닉네임 입력
+const pwInput = document.querySelector('.user-pw input'); //비번 입력
+let comments = []; //댓글 초기화
 
-// // // function commentBtnHandler(e) {
-// // //     e.preventDefault();
-// // //       console.dir(e.target);
-// // //       console.log(e.target[0]);
-// // //       console.log(e.target[0].value);
-// // //       console.log(e.target[0].element[0]);
-// // //       console.log(e.target[0].element[0].value);
-// // //       console.log(e.target[0].element.content);
-// // //       console.log(e.target[0].element.content.value);
-// // //       console.log(e.target.content);
-// // //       console.log(e.target.content.value);
+//이벤트를 등록 (생활코딩https://youtu.be/ncv3UwUarUc?si=ALWe4NOe3-QzVRc4)
+//댓글 데이터를 불러오고 데이터를 배열 형태로 변환하고 화면에 표시
+//window.addEventListener('load', () => {...}) : 나도 왜쓰는지는 모르겠지만 안정성이 올라간다고 함
+window.addEventListener('load', () => {
+  const storedComments = localStorage.getItem('comments');
+  if (storedComments) {
+    comments = JSON.parse(storedComments);
+    updateComments();
+  }
+});
+//댓글 입력 폼 함수
+function commentBtnHandler(e) {
+  e.preventDefault();
 
-// // // function commentBtnHandler(e) {
-// // //   e.preventDefault();
-// // //   list.push(e.target.content.value);
-// // //   return console.log(list);
-// // // }
+  const commentText = commentInput.value; //내용
+  const nickName = nickInput.value; //닉네임
+  const password = pwInput.value; //비번 type을 password로 변경하여 비번 느낌스
+  //!부정연산자 비어있음 =비어있음=비어있음
+  if (!commentText || !nickName || !password) {
+    alert('댓글 내용, 닉네임, 비밀번호를 모두 입력해 주세요!!.');
+    return;
+  }
+  //userid: Unknown word 자꾸 뜨는데 포기.....가 아닌 과제 제출 전까지 찾아보겠음
+  //댓글 생성
+  const newComment = {
+    userid: nickName,
+    content: commentText,
+    date: new Date().toLocaleString(),
+  };
+  //댓글 추가
+  comments.push(newComment);
+  //로컬 스토리지에 댓글 저장
+  //문자열로 변환하여 저장
+  localStorage.setItem('comments', JSON.stringify(comments));
 
-// // function commentBtnHandler(e) {
-// //   e.preventDefault();
-// //   const input = e.target.content;
-// //   if (input.value === "") {
-// //     alert("한글자 이상 입력해주세요");
-// //     return;
-// //   }
-// //   list.push(input.value);
-// // }
+  updateComments();
 
-// // commentBtn.addEventListener("submit", commentBtnHandler);
-// const commentBtn = document.querySelector("#commentFrm");
-// const list = [];
-
-// function Comment(content) {
-//     this.userid = "cloudcoke";
-//     this.content = content;
-//     this.date=Temporal.Now.timeZone()
-// }
-
-// function commentBtnHandler(e) {
-//   e.preventDefault();
-//   const input = e.target.content;
-//   if (input.value === "") {
-//     alert("한글자 이상 입력해주세요");
-//     return;
-//   }
-//   const commentObj = new Comment(input.value);
-//   list.push(input.value);
-//   e.target.reset();
-// }
-
-// const commentList = document.querySelector("#comment-list");
-// )
-
-// commentBtn.addEventListener("submit", commentBtnHandler);
-
-const commentBtn = document.querySelector('#commentFrm');
-const commentList = document.querySelector('#comment-list');
-const total = document.querySelector('h4 > span');
-const list = [];
-
-function Comment(content) {
-  this.userid = '방문자';
-  this.content = content;
-  this.date = new Date();
+  commentInput.value = '';
+  nickInput.value = '';
+  pwInput.value = '';
 }
+// 댓글 표시 업데이트 함수
+function updateComments() {
+  commentList.innerHTML = '';
+  // 댓글 최신순으로 !!!!!!!
+  comments.reverse();
 
-function createRow(userid, content, date) {
+  for (const comment of comments) {
+    const row = createRow(comment.userid, comment.content, comment.date);
+    commentList.appendChild(row);
+  }
+  //댓글의 갯수를 업데이트
+  total.innerText = comments.length;
+}
+// 이거는 진짜 모르겠음 컨닝.....
+function createRow(nickInput, content, date) {
   const ul = document.createElement('ul');
   const li1 = document.createElement('li');
   const li2 = document.createElement('li');
   const li3 = document.createElement('li');
 
-  ul.append(li1);
-  ul.append(li2);
-  ul.append(li3);
+  ul.appendChild(li1);
+  ul.appendChild(li2);
+  ul.appendChild(li3);
 
-  ul.setAttribute('class', 'comment-row');
-  li1.setAttribute('class', 'comment-id');
-  li2.setAttribute('class', 'comment-content');
-  li3.setAttribute('class', 'comment-date');
+  ul.className = 'comment-row';
+  li1.className = 'comment-id';
+  li2.className = 'comment-content';
+  li3.className = 'comment-date';
 
-  li1.innerHTML = userid;
+  li1.innerHTML = nickInput;
   li2.innerHTML = content;
   li3.innerHTML = date;
 
   return ul;
 }
+//새로운 댓글을 추가 등록 누를 때 마다
+commentBtn.addEventListener('click', commentBtnHandler);
 
-function drawing() {
-  commentList.innerHTML = '';
-  for (let i = list.length - 1; i >= 0; i--) {
-    const row = createRow(list[i].userid, list[i].content, list[i].date);
-    commentList.append(row);
-  }
-}
-
-function totalRecord() {
-  total.innerHTML = `(${list.length})`;
-}
-
-function commentBtnHandler(e) {
-  e.preventDefault();
-  const input = e.target.content;
-  if (input.value === '') {
-    alert('내용을 넣고 등록 버튼을 눌러주세요.');
-    return;
-  }
-  const commentObj = new Comment(input.value);
-  list.push(commentObj);
-  totalRecord();
-
-  drawing();
-  e.target.reset();
-}
-
-totalRecord();
-commentBtn.addEventListener('submit', commentBtnHandler);
+updateComments();
