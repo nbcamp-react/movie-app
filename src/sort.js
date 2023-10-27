fae1ae74ed2dd7c16be039e220d0af486771eac9
scseong <goidtv@naver.com>
2023-10-27 23:48:41 +0900

    feat: 영화 장르 필터 추가 (#33)

--- src/sort.js
+++ src/sort.js
@@ -29,50 +29,82 @@ export const filterAndSortMovieCards = () => {
   const $movieCards = document.querySelectorAll('.movie-card');
   let cardArr = [...$movieCards];
 
+  const genreFilter = new Set();
+  const ratingFilter = new Set();
+  const searchFilter = new Set();
+
+  $movieCards.forEach((card) => {
+    if (card.classList.contains('no-results')) searchFilter.add(card.id);
+  });
+
   if (order) {
-    cardArr
-      .sort((cardLi1, cardLi2) => {
-        switch (order) {
-          case SORTBY.TITLE:
-            const cardTitle1 = cardLi1
-              .querySelector('h2')
-              .innerText.toUpperCase();
-            const cardTitle2 = cardLi2
-              .querySelector('h2')
-              .innerText.toUpperCase();
+    cardArr.sort((cardLi1, cardLi2) => {
+      switch (order) {
+        case SORTBY.TITLE:
+          const cardTitle1 = cardLi1
+            .querySelector('h2')
+            .innerText.toUpperCase();
+          const cardTitle2 = cardLi2
+            .querySelector('h2')
+            .innerText.toUpperCase();
 
-            if (cardTitle1 < cardTitle2) return -1;
-            if (cardTitle1 > cardTitle2) return 1;
-            return 0;
+          if (cardTitle1 < cardTitle2) return -1;
+          if (cardTitle1 > cardTitle2) return 1;
+          return 0;
 
-          case SORTBY.RATING:
-            const cardRating1 = +cardLi1
-              .querySelector('h5')
-              .innerText.split(' ')[2];
-            const cardRating2 = +cardLi2
-              .querySelector('h5')
-              .innerText.split(' ')[2];
+        case SORTBY.RATING:
+          const cardRating1 = +cardLi1
+            .querySelector('h5')
+            .innerText.split(' ')[1];
+          const cardRating2 = +cardLi2
+            .querySelector('h5')
+            .innerText.split(' ')[1];
 
-            return cardRating2 - cardRating1;
+          return cardRating2 - cardRating1;
 
-          // case SORTBY.YEAR:
-          //   return;
-          default:
-            return;
-        }
-      })
-      .forEach((element) => $movieList.append(element));
+        default:
+          return;
+      }
+    });
+  }
+
+  if (genre) {
+    cardArr.forEach((card) => {
+      if (card.classList.contains('no-results')) return;
+
+      const movieGenres = card
+        .querySelector('h4')
+        .innerText.split(',')
+        .map((v) => v.trim());
+      movieGenres.includes(genre)
+        ? genreFilter.delete(card.id)
+        : genreFilter.add(card.id);
+    });
   }
 
   if (rating) {
     cardArr.forEach((card) => {
       if (card.classList.contains('no-results')) return;
 
-      const average = +card.querySelector('h5').innerText.split(' ')[2];
-
+      const average = +card.querySelector('h5').innerText.split(' ')[1];
       average >= rating
-        ? card.classList.remove('not-found')
-        : card.classList.add('not-found');
+        ? ratingFilter.delete(card.id)
+        : ratingFilter.add(card.id);
     });
   }
+
+  cardArr.forEach((element) => {
+    console.log(searchFilter, genreFilter, ratingFilter);
+    if (
+      searchFilter.has(element.id) ||
+      genreFilter.has(element.id) ||
+      ratingFilter.has(element.id)
+    ) {
+      element.classList.add('not-found');
+    } else {
+      element.classList.remove('not-found');
+    }
+
+    $movieList.append(element);
+  });
 };
